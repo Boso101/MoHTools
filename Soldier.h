685fbd66a999c4c369a939268f0d5bb78fe77b2c
Boso101 <bosorevived@gmail.com>
2021-06-03 13:57:28 +0930

    Update Soldier.h

--- Soldier.h
+++ Soldier.h
@@ -9,7 +9,7 @@ struct Soldier
   int unknownPointer1;
   int unkownPointer2;
   int unkownPointer3;
-  Variables* Variables; //0x0C Variable Pointer to health and stuff
+  Variables* variables; //0x0C Variable Pointer to health and stuff
   int unknown1;
   int unknown2;
   int unknown3;
