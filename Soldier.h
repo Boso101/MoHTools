3ee40acc2d0166d0000597982cc4964ef2fe2569
Boso101 <bosorevived@gmail.com>
2021-06-04 14:00:26 +0930

    Update Soldier.h

--- Soldier.h
+++ Soldier.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "Team.h"
-#define SOLDIER_SIZE 0xC0
+#define SOLDIER_SIZE 0xA640
 
 
 
@@ -20,8 +20,12 @@ struct Soldier
   int unknown5;
   int unknown6;
   Team currentTeam; //0x30 // Just one byte
-  int unknown7
-  
+  int unknown7;
+  char padding_0x2D90[0x2D90];
+ //For now just use float for each
+  float xPos;
+  float yPos;
+  float zPos;
   
   
 };
